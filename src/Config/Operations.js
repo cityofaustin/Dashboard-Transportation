068c233e0bec849e06a2299ab842eeceb51745ef
McKinley Forbes <solidaritydesignsva@gmail.com>
2018-03-30 13:16:15 -0500

    Commited wrong repo

---
+++ src/Config/Operations.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Card from "mapboard-components/build/Card";
+import {data} from './objects';
+
+class Operations extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      year : 2018
+    }
+
+  }
+
+  handleClick = e => {
+      e.preventDefault();
+      this.setState({
+          year: e.target.value
+      })
+  };
+  render() {
+    return (
+      <div className="App">
+          {data.map((item, index) => {
+              return <Card year={this.state.year} config={item} key={index}/>
+          })}
+              <ul>
+                  <li value={2016} onClick={this.handleClick}>2016</li>
+                  <li value={2017} onClick={this.handleClick}>2017</li>
+                  <li value={2018} onClick={this.handleClick}>2018</li>
+              </ul>
+      </div>
+    );
+  }
+}
+
+export default Operations;
